d8aecd5c91fc195651e75068927611b27b99e9f1
blackburnt2018 <tblackburn@sjnma.org>
2018-02-20 14:47:16 -0600

    pypractice

---
+++ flow.py
@@ -0,0 +1,11 @@
+print("this is great")
+response = raw_input("How are you")
+if(response.lower() == "good"):
+	print("totally great")
+	print("agreed")
+elif(response.lower() == "ok"):
+	print("Okay, I understand")
+elif(response.lower() == "bad"):
+	print("... haaalp")
+else:
+	print("what was that? I don't understand what you said")
